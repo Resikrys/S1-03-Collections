b1ef9a35ad31a2fa45c95ed6a2ddb26cd9bc722c
resikrys <resicris@gmail.com>
2025-05-22 11:24:03 +0200

    conversion arrayList to hashSet

--- src/s1_01_collections_lvl1_ex1/Main.java
+++ src/s1_01_collections_lvl1_ex1/Main.java
@@ -1,11 +1,13 @@
 package s1_01_collections_lvl1_ex1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Month> months = new ArrayList<Month>();
 
+        //Create & Add month to ArrayList
         months.add(new Month("January"));
         months.add(new Month("February"));
         months.add(new Month("March"));
@@ -18,11 +20,29 @@ public class Main {
         months.add(new Month("November"));
         months.add(new Month("December"));
 
+        //ArrayList - adding august in i[7]
         Month august = new Month("August");
         months.add(7, august);
 
         System.out.println(months.get(7).getName());
         System.out.println(months.toString());
+
+        HashSet<Month> hashMonths = new HashSet<>(months);
+
+        //How to assure no duplicates in the hashSet
+        if(hashMonths.contains(august)) {
+            System.out.println("This month already exists in the list");
+        } else {
+            hashMonths.add(august);
+        }
+
+        //For loop
+        for (Month monthName : hashMonths) {
+            System.out.println(monthName);
+        }
+
+        //Iterator
+
 
     }
 }
