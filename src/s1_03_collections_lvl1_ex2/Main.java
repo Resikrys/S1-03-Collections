fd4f7cca8922f714850f24f2015c13769e071659
resikrys <resicris@gmail.com>
2025-05-22 13:00:11 +0200

    reversed Integer list1 into Integer list2

--- src/s1_03_collections_lvl1_ex2/Main.java
+++ src/s1_03_collections_lvl1_ex2/Main.java
@@ -1,7 +1,33 @@
 package s1_03_collections_lvl1_ex2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<Integer>();
+        List<Integer> reversedList = new ArrayList<Integer>();
+
+        //myFirstList.add(2, 13, 213, 7, 1412, 20);
+        Collections.addAll(myList, 2, 13, 213, 7, 1412, 20);
+
+        System.out.println("ArrayList Elements in Normal Order");
+        Iterator<Integer> myListIterator = myList.iterator();
+
+        while(myListIterator.hasNext()) {
+            System.out.println("" + myListIterator.next());
+        }
+
+        System.out.println("ArrayList Elements in Reversed Order");
+        for(int i = myList.size(); i > 0; i--) {
+            reversedList.add(myList.size() - i, myList.get(i - 1));
+        }
+
+        for(Integer invertedValue : reversedList) {
+            System.out.println("" + invertedValue);
+        }
 
     }
 }
