df7d6c5b50b82b9bad54b971ac46e4219d89e3b4
resikrys <resicris@gmail.com>
2025-05-22 10:26:46 +0200

    add Month class

--- src/s1_01_collections_lvl1_ex1/Month.java
+++ src/s1_01_collections_lvl1_ex1/Month.java
@@ -1,4 +1,17 @@
 package s1_01_collections_lvl1_ex1;
 
 public class Month {
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
