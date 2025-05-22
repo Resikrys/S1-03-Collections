17f4387461c3c17521beeefcfb408b5fe4e359c9
resikrys <resicris@gmail.com>
2025-05-22 10:50:05 +0200

    add main with arrayList months & add august at [i = 7]

---
+++ src/s1_01_collections_lvl1_ex1/Main.java
@@ -0,0 +1,27 @@
+package s1_01_collections_lvl1_ex1;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Month> months = new ArrayList<Month>();
+
+        months.add(new Month("January"));
+        months.add(new Month("February"));
+        months.add(new Month("March"));
+        months.add(new Month("April"));
+        months.add(new Month("May"));
+        months.add(new Month("June"));
+        months.add(new Month("July"));
+        months.add(new Month("September"));
+        months.add(new Month("October"));
+        months.add(new Month("November"));
+        months.add(new Month("December"));
+
+        Month august = new Month("August");
+        months.add(7, august);
+
+        System.out.println(months.get(7).getName());
+
+    }
+}
