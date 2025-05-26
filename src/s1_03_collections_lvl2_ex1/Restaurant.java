3b609ca33750ba79597f21032c06c2cc7afd08c5
resikrys <resicris@gmail.com>
2025-05-26 12:38:12 +0200

    add restaurant class

---
+++ src/s1_03_collections_lvl2_ex1/Restaurant.java
@@ -0,0 +1,27 @@
+package s1_03_collections_lvl2_ex1;
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getScore(int score) {
+        this.score = score;
+    }
+}
