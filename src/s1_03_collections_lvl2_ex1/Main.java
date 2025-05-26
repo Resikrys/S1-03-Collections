6108febcaaac89000afca2e8e236a41cc0bbdb56
resikrys <resicris@gmail.com>
2025-05-26 18:03:53 +0200

    upd main file: add sorted restaurant list method

--- src/s1_03_collections_lvl2_ex1/Main.java
+++ src/s1_03_collections_lvl2_ex1/Main.java
@@ -1,11 +1,10 @@
 package s1_03_collections_lvl2_ex1;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
     public static void main(String [] args) {
-        //Compare new obj with set objs (if exists, not add - catch error-, if not, add in the set)
-        HashSet<Restaurant> restaurants = new HashSet<Restaurant>();
+        HashSet<Restaurant> restaurants = new HashSet<Restaurant>(); //unordered
 
         restaurants.add(new Restaurant("Manolo's", 3));
         restaurants.add(new Restaurant("Luigi's Mansion", 9));
@@ -47,11 +46,23 @@ public class Main {
             System.out.println("❌ " + e.getMessage());
         }
 
-//        System.out.println(restaurants.toString());
-
         System.out.println("\n📋 Restaurant list:");
         for (Restaurant r : restaurants) {
             System.out.println(" - " + r);
         }
+
+        //EXERCISE 2: add descendant list sort
+        List<Restaurant> restaurantList = new ArrayList<>(restaurants); //ordered list
+
+        restaurantList.sort(
+                Comparator.comparing(Restaurant::getName, Comparator.reverseOrder()) //Order from Z to A
+                        .thenComparing(Comparator.comparing(Restaurant::getScore).reversed()) //Order from 10 to 0
+        );
+
+        System.out.println("\n📋 Sorted restaurant list:");
+        for (Restaurant r : restaurantList) {
+            System.out.println(r);
+        }
+
     }
 }
--- src/s1_03_collections_lvl2_ex1/Restaurant.java
+++ src/s1_03_collections_lvl2_ex1/Restaurant.java
@@ -23,7 +23,7 @@ public class Restaurant {
         this.name = name;
     }
 
-    public void getScore(int score) {
+    public void setScore(int score) {
         this.score = score;
     }
 
